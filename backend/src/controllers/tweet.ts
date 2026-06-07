0e17ca3d5b04fb3beb519d98f6680a5762abbe58
Neel Prajapati <121471469+Neel26022@users.noreply.github.com>
2026-06-07 05:28:37 +0000

    show tweet and delete  tweet is completed

--- backend/src/controllers/tweet.ts
+++ backend/src/controllers/tweet.ts
@@ -71,7 +71,72 @@ const showTweet = async (req: Request, res: Response) => {
     }
 }
 
+const deleteTweet = async (req:Request, res:Response) => {
+    const tweetId = req.params.tweetId
+    const userId = req.user['userId']
+
+    try {
+        if(!tweetId || !userId) {
+            return res.status(400).json({
+                message: "tweetId or userId is not find"
+            })
+        }
+
+        const deletedTweet = await Tweet.deleteOne({
+            userId: userId,
+            tweeetId: tweetId
+        })
+
+        if(!deletedTweet) {
+            return res.status(400).json({
+                message: "tweet is not deleted"
+            })
+        }
+
+        res.status(200).json({
+            message: "Tweet is deleted",
+            data: deletedTweet
+        })
+    } catch (e) {
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+}
+const showOwnTweet = async (req:Request, res:Response) => {
+    const userId = req.user['userId']
+
+    try {
+        if(!!userId) {
+            return res.status(400).json({
+                message: "tweetId or userId is not find"
+            })
+        }
+
+        const ownTweet = await Tweet.find({
+            userId: userId
+        })
+
+        if(!ownTweet) {
+            return res.status(400).json({
+                message: "tweet is not findd"
+            })
+        }
+
+        res.status(200).json({
+            message: "Tweet is finded",
+            data: ownTweet
+        })
+    } catch (e) {
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+}
+
 export {
     createTweet,
-    showTweet
+    showTweet,
+    deleteTweet,
+    showOwnTweet
 }
--- backend/src/routes/tweets.route.ts
+++ backend/src/routes/tweets.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTweet, showTweet } from "../controllers/tweet.js";
+import { createTweet, deleteTweet, showOwnTweet, showTweet } from "../controllers/tweet.js";
 import auth from "../middleware/auth.js";
 import { createComment, showComment } from "../controllers/comment.js";
 import { createReplay, showReplay } from "../controllers/replay.js";
@@ -18,5 +18,9 @@ tweetRouter.get("/:tweetId/comment",auth, showComment)
 tweetRouter.get("/:tweetId/replay", auth, showReplay)
 
 tweetRouter.post("/:tweetId/:commentId/replay",auth, createReplay)
+
+tweetRouter.get('/own-tweet', auth, showOwnTweet)
+
+tweetRouter.delete('/tweet-delete', auth, deleteTweet)
 
 export default tweetRouter
